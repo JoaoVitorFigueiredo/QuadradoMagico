a5b9136349a8858add4a8bdd0f9818432ddf8486
JoaoVitorFigueiredo <jvlfo@iscte-iul.pt>
2025-04-07 22:54:59 +0100

    Crossover one point e two point implementados

--- Individual.py
+++ Individual.py
@@ -1,11 +1,32 @@
+import random
+
 from Utils import Utils
 
 
 class Individual:
     def __init__(self, cube: list):
         self.__cube = cube
-        self.__loss = Utils.loss_function(cube)
+        self.__loss = Utils.fitness_function(cube)
 
     def get_cube(self):
         return self.__cube
+
+    def mutation(self):
+        idx = range(len(self.__cube))
+        i1, i2 = random.sample(idx, 2)
+        self.__cube[i1], self.__cube[i2] = self.__cube[i2], self.__cube[i1]
+
+    def __str__(self):
+        return self.__cube.__str__()
+
+
+if __name__ == "__main__":
+    individual = Individual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    individual.mutation()
+    print(individual)
+    individual.mutation()
+    print(individual)
+    individual.mutation()
+    print(individual)
+
 
--- Utils.py
+++ Utils.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
+
 from statistics import mean
+
+import random
+
+import Individual
 
 
 class Utils:
     @staticmethod
-    def loss_function(cube: list):
+    def fitness_function(cube: list):
         """
 
         :param cube:
@@ -13,6 +19,7 @@ class Utils:
         6,7,8]
 
         0+3+6 = 1+4+7 = 2+5+8 = 0+1+2 = 3+4+5 = 6+7+8
+
 
         :return:
         """
@@ -26,9 +33,42 @@ class Utils:
         mean_sum = mean(results)
 
         loss = 0
+
         for result in results:
             if (result-mean_sum) > 0:
                 loss += result-mean_sum
             else:
                 loss -= result-mean_sum
         return loss
+
+    @staticmethod
+    def crossover_one_point(ind1: "Individual", ind2: "Individual"):
+        crossover_point = random.randint(1, 8)
+        new_individual_cube = []
+        for i in range(crossover_point):
+            new_individual_cube.append(ind1.get_cube()[i])
+        for i in range(crossover_point, 9):
+            new_individual_cube.append(ind2.get_cube()[i])
+
+        return Individual.Individual(new_individual_cube)
+
+    @staticmethod
+    def crossover_two_point(ind1: "Individual", ind2: "Individual"):
+        crossover_point_1 = random.randint(1, 6)
+        crossover_point_2 = random.randint(crossover_point_1+2,9)
+
+        new_individual_cube = []
+
+        for i in range(crossover_point_1):
+            new_individual_cube.append(ind1.get_cube()[i])
+        for i in range(crossover_point_1, crossover_point_2):
+            new_individual_cube.append(ind2.get_cube()[i])
+        for i in range(crossover_point_2,9):
+            new_individual_cube.append(ind1.get_cube()[i])
+
+        return Individual.Individual(new_individual_cube)
+
+
+if __name__ == "__main__":
+
+    pass
