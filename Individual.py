3dafa7177d893aa94861f86e7798e358aa244f74
dudocas12 <edmse@iscte-iul.pt>
2025-04-20 11:30:02 +0100

    mudanças de 9 para n

--- Individual.py
+++ Individual.py
@@ -1,123 +1,84 @@
 import random
-from statistics import mean
-
 
 class Individual:
-    def __init__(self, cube: list):
-        self.__cube = cube
-        self.__fitness = IndividualUtils.fitness_function(self)
+    def __init__(self, cube: list, order: int = 3):
+        """
+        :param cube: flat list of length order*order representing the square
+        :param order: dimension n of the square (n×n)
+        """
+        assert len(cube) == order * order, f"Expected {order*order} values, got {len(cube)}"
+        self.order = order
+        self._cube = cube[:]  # copy
+        self._fitness = IndividualUtils.fitness_function(self)
 
     def get_cube(self):
-        return self.__cube
+        return self._cube[:]
 
-    def set_cube(self, new_cube=list):
-        self.__cube = new_cube
+    def set_cube(self, new_cube: list):
+        assert len(new_cube) == self.order * self.order
+        self._cube = new_cube[:]
+        self._fitness = IndividualUtils.fitness_function(self)
 
     def get_fitness(self):
-        return self.__fitness
+        return self._fitness
 
     def mutation(self):
-        idx = range(len(self.__cube))
-        i1, i2 = random.sample(idx, 2)
-        self.__cube[i1], self.__cube[i2] = self.__cube[i2], self.__cube[i1]
+        # swap two genes and recompute fitness
+        i1, i2 = random.sample(range(self.order * self.order), 2)
+        self._cube[i1], self._cube[i2] = self._cube[i2], self._cube[i1]
+        self._fitness = IndividualUtils.fitness_function(self)
 
     def __str__(self):
-        return self.__cube.__str__()
+        # pretty-print rows
+        rows = [self._cube[i*self.order:(i+1)*self.order] for i in range(self.order)]
+        return "\n".join(str(r) for r in rows)
 
     def __gt__(self, other):
-        return self.get_cube() >= other.get_cube()
+        return self.get_fitness() > other.get_fitness()
 
-    def __int__(self):
-        return self.get_cube()
+    def __lt__(self, other):
+        return self.get_fitness() < other.get_fitness()
 
 
 class IndividualUtils:
     @staticmethod
     def fitness_function(ind: Individual):
         """
-
-        :param ind:
-
-        [0,1,2,
-        3,4,5
-        6,7,8]
-
-        0+3+6 = 1+4+7 = 2+5+8 = 0+1+2 = 3+4+5 = 6+7+8
-
-
-        :return:
+        Fitness = -sum(|sum(row/col/diag) - magic|), where magic = n*(n^2+1)/2
+        Perfect square => fitness = 0
         """
+        n = ind.order
         cube = ind.get_cube()
+        # build n×n matrix
+        grid = [cube[i*n:(i+1)*n] for i in range(n)]
+        magic = n * (n*n + 1) // 2
 
-        results = [cube[0] + cube[3] + cube[6],
-                   cube[1] + cube[4] + cube[7],
-                   cube[2] + cube[5] + cube[8],
-                   cube[0] + cube[1] + cube[2],
-                   cube[3] + cube[4] + cube[5],
-                   cube[6] + cube[7] + cube[8]]
+        errors = []
+        # rows & cols
+        for i in range(n):
+            errors.append(abs(sum(grid[i]) - magic))
+            errors.append(abs(sum(grid[r][i] for r in range(n)) - magic))
+        # diags
+        errors.append(abs(sum(grid[i][i] for i in range(n)) - magic))
+        errors.append(abs(sum(grid[i][n-1-i] for i in range(n)) - magic))
 
-        mean_sum = mean(results)
-
-        loss = 0
-
-        for result in results:
-            if (result - mean_sum) < 0:
-                loss += result - mean_sum
-            else:
-                loss -= result - mean_sum
-        return loss
+        loss = sum(errors)
+        return -loss
 
     @staticmethod
-    def crossover_one_point(ind1: "Individual", ind2: "Individual"):
-        crossover_point = random.randint(1, 8)
-        new_individual_cube = []
-        for i in range(crossover_point):
-            new_individual_cube.append(ind1.get_cube()[i])
-        for i in range(crossover_point, 9):
-            new_individual_cube.append(ind2.get_cube()[i])
-
-        return Individual(new_individual_cube)
+    def crossover_one_point(ind1: Individual, ind2: Individual):
+        n2 = ind1.order * ind1.order
+        p = random.randint(1, n2-1)
+        head = ind1.get_cube()[:p]
+        tail = [x for x in ind2.get_cube() if x not in head]
+        return Individual(head + tail, order=ind1.order)
 
     @staticmethod
     def crossover_two_point(ind1: Individual, ind2: Individual):
-        crossover_point_1 = random.randint(1, 6)
-        crossover_point_2 = random.randint(crossover_point_1 + 2, 9)
-
-        new_individual_cube = []
-
-        for i in range(crossover_point_1):
-            new_individual_cube.append(ind1.get_cube()[i])
-        for i in range(crossover_point_1, crossover_point_2):
-            new_individual_cube.append(ind2.get_cube()[i])
-        for i in range(crossover_point_2, 9):
-            new_individual_cube.append(ind1.get_cube()[i])
-
-        return Individual(new_individual_cube)
-
-    @staticmethod
-    def mutation(ind: Individual):
-        cube = ind.get_cube()
-
-        gene_1_index = random.randint(0, 8)
-        gene_2_index = random.randint(0, 8)
-
-        while gene_1_index == gene_2_index:
-            gene_2_index = random.randint(0, 8)
-
-        cube[gene_1_index], cube[gene_2_index] = cube[gene_2_index], cube[gene_1_index]
-
-        ind.set_cube(cube)
-
-
-if __name__ == "__main__":
-    cubo_teste_2 = [2, 2, 9, 3, 9, 9, 2, 2, 4]
-    cubo_teste_1 = [1, 3, 5, 7, 9, 11, 13, 15, 17]
-
-    ind_1 = Individual(cubo_teste_1)
-    ind_2 = Individual(cubo_teste_2)
-
-    print(ind_2.get_fitness())
-
-    print(IndividualUtils.crossover_one_point(ind_1, ind_2))
-
-    pass
+        n2 = ind1.order * ind1.order
+        p1 = random.randint(0, n2-2)
+        p2 = random.randint(p1+1, n2-1)
+        slice_ = ind1.get_cube()[p1:p2]
+        rest = [x for x in ind2.get_cube() if x not in slice_]
+        child = rest[:p1] + slice_ + rest[p1:]
+        return Individual(child, order=ind1.order)
